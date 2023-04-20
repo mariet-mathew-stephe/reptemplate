595da4ce64aad3d5a8df197cc047a6661d658b3d
mariet-mathew-stephe <mariet.stephen@gmail.com>
2023-04-20 11:23:29 -0600

    installing "available" package

--- R/hello.R
+++ R/hello.R
@@ -1,5 +1,6 @@
 # checking package name availability
 
+# install.packages("available")
 library(available)
 library(Matrix)
 available::suggest("create read to use pdf template for o2 statistical analysis reports ")
@@ -9,3 +10,4 @@ available::available("reptemplate", browse = FALSE)
 
 # The following function runs a local R CMD check
 devtools::check()
+
